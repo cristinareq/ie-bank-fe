caa1c411004ef6475b6c14b4fda2961b50ee8f56
cristina requena <crequena.ieu2021@student.ie.edu>
2023-10-10 10:50:34 +0200

    adding skull

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,12 +1,22 @@
 <template>
     <div>
+      <div>
         <h1>Home Page</h1>
         <p>Welcome to our page!! </p>
-        <p> Have fun dying on vscode</p>
         <router-link to="/accounts">
       <button>View Accounts</button>
+      
     </router-link>
 
     </div>
+    <div>
+        <router-link to="/skull">
+      <button>Skull</button>
+      
+    </router-link>
+
+    </div>
+    </div>
+    
 </template>
  
